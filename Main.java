8def0d6e3012b2f11ec935812881bedf88ee06cc
Cameron East <114606828+cameroneast@users.noreply.github.com>
2023-01-17 19:16:05 +0000

    fixed accessibility of class variables in MyArrayList and tested a few methods.

--- Main.java
+++ Main.java
@@ -3,12 +3,23 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello. How many tickets would you like to be available for purchase?");
     Scanner scnr = new Scanner(System.in);
-    MyArrayList[] tickets = new MyArrayList[scnr.nextInt()];
+    MyArrayList tickets = new MyArrayList(scnr.nextInt());
 
+    //Test the size method
+    System.out.println("You want " + tickets.size() + " tickets.");
 
+    //Test the get and set item methods.
+    System.out.println("Select ticket number: ");
+    int ticketNum = scnr.nextInt();
+    
+    System.out.println("Enter value for the ticket number: ");
+    int ticketVal = scnr.nextInt();
 
+    tickets.setItem(ticketNum - 1, ticketVal);
 
+    System.out.println("Ticket number " + ticketNum + " has a value of " + tickets.getItem(ticketNum - 1));
 
+    
 
     scnr.close();
   }
--- MyArrayList.java
+++ MyArrayList.java
@@ -1,18 +1,23 @@
 class MyArrayList {
-  int size = 0;
-  int[] items;
+  private int size = 0;
+  private int[] items;
 
   public MyArrayList(int size) {
     this.size = size;
-    items = new int[size];
+    this.items = new int[size];
   }
 
-  public void setItem(int index, int newItem){
-    items[index] = newItem;
+  public void setItem(int index, int newItem) {
+    this.items[index] = newItem;
   }
 
   public int getItem(int index){
-    return items[index];
+    if(index<0 || index > (this.items.length -1)){
+      return -1;
+    }
+    else{
+      return this.items[index];
+    }
   }
 
   public int size() {
@@ -23,19 +28,34 @@ class MyArrayList {
     size++;
     int[] a = new int[size];
     for(int i = 0; i<size-1; i++){
-      a[i] = items[i];
+      a[i] = this.items[i];
     }
     a[size-1] = newItem;
+    this.items = new int[size];
+    for(int i = 0; i<size; i++){
+      this.items[i] = a[i];
+    }
   }
 
-  public void removeItem(int index){
-    size--;
-    int[] b = new int[size];
-    for(int i = 0; i<index; i++){
-      b[i] = items[i];
+  public int removeItem(int index){
+    if(index > this.items.length || index<0){
+      return -1;
     }
-    for(int i = index; i<size-1; i++){
-      b[i] = items[i+1];
+    else{
+      size--;
+      int[] b = new int[size];
+      int element = items[index];
+      for(int i = 0; i<index; i++){
+        b[i] = this.items[i];
+      }
+      for(int i = index; i<size; i++){
+        b[i] = this.items[i+1];
+      }
+      this.items = new int[size];
+      for(int i = 0; i<size; i++){
+        this.items[i] = b[i];
+      }
+      return element;
     }
   }
 
@@ -43,16 +63,20 @@ class MyArrayList {
     //Declare new array to temporarily store the exisiting values and expand.
     int[] newArr = new int[newsize];
     
+    //Populate temporary array with existing values.
     for (int i = 0; i < size; i++) {
-      newArr[i] = items[i];
+      newArr[i] = this.items[i];
     }
-    
+
+    //Set the size to the user-defined size.
     size = newsize;
+    
     //Initialize items as a new array with new size.
-    items = new int[size];
+    this.items = new int[size];
+    
     //Copy values from temporary array to the items array.
     for(int i = 0; i < size; i++) {
-      items[i] = newArr[i];
+      this.items[i] = newArr[i];
     }
     
   }
